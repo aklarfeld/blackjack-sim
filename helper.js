8fb33fcf3e8023bfe0bbc416adbcbdaf9bebadc5
aklarfeld <alexklarfeld@gmail.com>
2021-12-28 09:44:35 -0800

    Saving status

--- helper.js
+++ helper.js
@@ -8,6 +8,13 @@ const getValue = (cards) => {
     return values;
 }
 
+const getCount = (card) => {
+    if(card.value <= 6) return 1;
+    if(card.value <= 9 && card.value >= 7) return 0;
+    if(card.value >= 10) return -1;
+}
+
 module.exports = {
     getValue,
+    getCount,
 }
